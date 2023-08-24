0e0dca6ec3ae3e6c000c4e3c1ae94149ba7679ba
simamumu89 <142807674+simamumu89@users.noreply.github.com>
2023-08-24 14:23:00 +0900

    Add files via upload

---
+++ Helloworld/src/com/example/helloworld.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class helloworld {
+    public static void main(String[] args) {
+        for (int i = 1; i < 15; i++)
+            if (i < 10) {
+                System.out.println(i + "; Hello world.");
+            } else {
+                System.out.println(i + "; world.");
+                
+            }
+        }
+    }
